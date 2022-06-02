771b947353b55823d5a3b47283a144e76c636a68
Alex Meyers <meyersam0427@gmail.com>
2022-06-01 19:31:21 -0500

    Add RESTful products action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/all_products", controller: "products", action: "all_products"
+  get "/products", controller: "products", action: "index"
   get "one_product/:id", controller: "products", action: "one_product"
 end
